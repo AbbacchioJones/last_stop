98d3bab66531d22f3d12394fc03ba300a1ce2bbd
Dillon Hubble <dillonhubble@gmail.com>
2017-04-27 20:28:31 -0700

    Renamed css and js, split location and contact from home into new page, added about us section on homepage[D[D[D[D[D[D[D[D[D[D[D[D and photos on homepage

--- js/laststop.js
+++ js/laststop.js
@@ -6,20 +6,20 @@ function contains the information the generate and center a google map at the lo
 the university of Washington, Tacoma
 */
 function init() {
-    var map = new google.maps.LatLng(47.2554865, -122.47653350000002);
+  var map = new google.maps.LatLng(47.2554865, -122.47653350000002);
 
-    var mapOptions = { // Set up the map options
-        center: map,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 16
-    };
-    var venueMap; // Map() draws a map
-    venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var mapOptions = { // Set up the map options
+    center: map,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 16
+  };
+  var venueMap; // Map() draws a map
+  venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    var startPosition = new google.maps.Marker({ // Create a new marker
-        position: map, // Set its position
-        map: venueMap, // Specify the map
-    });
+  var startPosition = new google.maps.Marker({ // Create a new marker
+    position: map, // Set its position
+    map: venueMap, // Specify the map
+  });
 }
 
 /*
@@ -28,9 +28,9 @@ function contains the begining information to pull information from a javscript 
 function "init" which displays a google map centered at the corrdiantes for the university of Washington, Tacoma
 */
 function loadScript() {
-    var script = document.createElement('script'); // Create <script> element
-    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
-    document.body.appendChild(script); // Add element to page
+  var script = document.createElement('script'); // Create <script> element
+  script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+  document.body.appendChild(script); // Add element to page
 }
 
 
@@ -50,12 +50,12 @@ window.onresize = align;
 function align() {
 
 
-//calculates height percentage of logo and navbar and adds them together to set the logocontainer height
+  //calculates height percentage of logo and navbar and adds them together to set the logocontainer height
   var logoheight = 100 / (window.innerHeight / document.getElementById('logoimg').clientHeight);
   var buttonheight = 100 / (window.innerHeight / document.getElementById('navbar').clientHeight);
-  document.getElementById('logocontainer').style.height = (logoheight + buttonheight)+ '%';
+  document.getElementById('logocontainer').style.height = (logoheight + buttonheight) + '%';
 
-//finds height of logocontainer and navbar and makes half the navbar extend beyond the logocontainer
+  //finds height of logocontainer and navbar and makes half the navbar extend beyond the logocontainer
   var containerheight = document.getElementById('logocontainer').clientHeight;
   var navbarheight = document.getElementById('navbar').clientHeight;
   document.getElementById('navbar').style.top = (containerheight - (navbarheight / 2)) + 'px';
